ee6eaff622e513c820f9299b4d4a16cd5e8967c8
suhibabbas <sohaib_salah96@yahoo.com>
2022-04-05 20:35:27 +0300

    all users are renderd

--- CodeFellowship/src/main/java/com/lab/CodeFellowship/Security/WebSecurityConfig.java
+++ CodeFellowship/src/main/java/com/lab/CodeFellowship/Security/WebSecurityConfig.java
@@ -53,14 +53,35 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .permitAll();
 //
 //    }
+//@Override
+//protected void configure(HttpSecurity http) throws Exception {
+//    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//            .disable()
+//            .csrf().disable()
+//            .authorizeRequests()
+//            .antMatchers("/login", "/signup").permitAll()
+//            .anyRequest().authenticated()
+//            .and()
+//            .formLogin()
+//            .loginPage("/login")
+//            .loginProcessingUrl("/perform_login")
+//            .defaultSuccessUrl("/", true)
+//            .failureUrl("/error")
+//            .and()
+//            .logout()
+//            .logoutUrl("/perform_logout")
+//            .deleteCookies("JSESSIONID");
+//}
 @Override
 protected void configure(HttpSecurity http) throws Exception {
-    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-            .disable()
+    http
+            .cors().disable()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/login", "/signup").permitAll()
-            .anyRequest().authenticated()
+            .antMatchers("/login", "/signup")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
             .and()
             .formLogin()
             .loginPage("/login")
